c042f8f25f7d2c29b75865f0b3abdf144d3644ce
Seshien <maciek22999@wp.pl>
2019-04-26 13:18:56 +0200

    Something happened

--- Main.py
+++ Main.py
@@ -1,8 +1,26 @@
+import Ball
+import PyQt5
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QPainter, QColor, QPolygon
+import sys
+
+
+
+class Window(QWidget):
+    def __init__(self, parent=None):
+        super().__init__()
+        self.interfejs()
+
+    def interfejs(self):
+        self.resize(800,600)
+        self.setWindowTitle("Simulation")
+        self.show()
+
 def main():
-    pass
-    return
+    app = QApplication(sys.argv)
+    okno = Window()
+    sys.exit(app.exec_())
 
 
-
-if __name__=="main":
+if __name__=="__main__":
     main()

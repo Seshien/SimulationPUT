c178725c7bdd331a309e84f9431d28f01d2f9c87
Michał <soulrpgmaker@gmail.com>
2019-04-26 12:55:18 +0200

    Ball v2

--- Ball.py
+++ Ball.py
@@ -1,6 +1,14 @@
 class Ball():
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.dx = 0
-        self.dy = 0
+    def __init__(self, xPos = 0, yPos = 0, r = 0, xSpeed = 0, ySpeed = 0):
+        self.x = xPos
+        self.y = yPos
+        self.radius = r
+        self.dx = xSpeed
+        self.dy = ySpeed
+    def ruch(self):
+        self.x += self.dx
+        self.y += self.dy
+    def collision(self, other, delta):
+        odlegloscX = abs(self.x + other.x)
+        odlegloscY = abs(self.y + other.y)
+        

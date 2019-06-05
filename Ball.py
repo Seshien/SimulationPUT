912b38a72850adae72722749951fff8342155017
Seshien <maciek22999@wp.pl>
2019-06-05 19:11:30 +0200

    Refresh working

--- Ball.py
+++ Ball.py
@@ -3,37 +3,12 @@ import random
 from tkinter import ttk
 from tkinter import Canvas
 
-class Ball():
-    # Wczytuje obrazek czasteczki
-    #czasteczka_img = tk.PhotoImage(file="czasteczka.png")
-
-    def __init__(self, xPos = 0, yPos = 0, r = 0, xSpeed = 0, ySpeed = 0,canvas=""):
-        self.x = xPos
-        self.y = yPos
-        self.radius = r
-        self.dx = xSpeed
-        self.dy = ySpeed
-        print("x")
-        self.img = canvas.create_image(self.x, self.y, image=tk.PhotoImage(file="czasteczka.png"))
-
-    def ruch(self):
-        self.x += self.dx
-        self.y += self.dy
-
-    def draw (self,window,canvas):
-        canvas.move(self.img,self.dx,self.dy)
-        #canvas.after(50, self.draw(window,canvas))
-
-    def collision(self, other, delta):
-        odlegloscX = abs(self.x + other.x)
-        odlegloscY = abs(self.y + other.y)
-
 class Ball2:
-    def __init__(self, canvas, x1, y1, x2, y2):
+    def __init__(self, canvas, x1, y1):
         self.x1 = x1
         self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
+        self.x2 = x1+5
+        self.y2 = y1+5
         self.canvas = canvas
         self.ball = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill="red")
 
@@ -41,4 +16,4 @@ class Ball2:
         deltax = random.randint(-5,5)
         deltay = random.randint(-5,5)
         self.canvas.move(self.ball, deltax, deltay)
-        self.canvas.after(50, self.move_ball)
+
--- tkinter_version/main.py
+++ tkinter_version/main.py
@@ -11,13 +11,14 @@ WIDTH = 800
 HEIGHT= 640
 # Czy mozna rozszerzac
 RESIZABLE = 0
+
+
 class Simulation:
     def __init__(self):
         # Lista wszystkich czasteczek
         self.particles = []
+        self.running=0
         self.create_window()
-        self.window.mainloop()
-
 
     def create_window(self):
         # Tworzy nowe okno klasy Tk
@@ -36,8 +37,11 @@ class Simulation:
         self.canvas = Canvas(self.window, width=WIDTH - 20, height=HEIGHT - 80, bg="#4bf2a7", borderwidth=2, relief="ridge")
         # Umieszczenia miejsca w oknie
         self.canvas.pack()
-        start = ttk.Button(text="Twórz",
-                           command=lambda: self.button_click())
+        add = ttk.Button(text="Dodaj",
+                           command=lambda: self.add_click(number_input.get()))
+        add.pack(side="right", padx=50)
+        start = ttk.Button(text="Start/Reset",
+                           command=lambda: self.start_click())
         start.pack(side="right", padx=50)
         text_ilosc = ttk.Label(self.window, text="Ilość cząsteczek:")
         text_ilosc.pack(side="left", padx=50)
@@ -45,12 +49,39 @@ class Simulation:
         number_input = ttk.Entry(self.window)
         number_input.pack(side="left")
         number_input.insert(0, "10")
+        self.refresh()
+        self.window.mainloop()
 
-    def button_click(self):
-        print("Something")
+    def add_click(self,number):
+        print("Add clicked")
+        self.add_new_particles(number)
 
-def create_random(particles, window,canvas):
-     particles.append(Ball(random.randrange(0, WIDTH-20), random.randrange(0, HEIGHT-80), 5, random.randrange(-5, 5), random.randrange(-5, 5),canvas))
+    def start_click(self):
+        print("Start clicked")
+        if self.running == 0:
+            self.running = 1
+
+        elif self.running == 1:
+            self.running = 0
+
+    def add_new_particles(self,number):
+        try:
+            number_of_particles = int(number)
+        except ValueError:
+            # Jesli nie to pokazuje blad i konczy prace programu
+            messagebox.showerror("Błąd!", "Jako ilość cząsteczek nie podano wartości liczbowej!")
+            self.window.destroy()
+        # Na razie tylko wyswietla, to trzeba bedzie zastapic tworzeniem obiektow danej klasy
+        for i in range(number_of_particles):
+            temp1 = random.randrange(0, WIDTH - 20)
+            temp2 = random.randrange(0, HEIGHT - 80)
+            self.particles.append(Ball2(self.canvas, temp1, temp2))
+
+    def refresh(self):
+        self.canvas.after(50, self.refresh)
+        if self.running==1:
+            for ball in self.particles:
+                ball.move_ball()
 
 def redraw(window,canvas,particles):
     print("Tada")
